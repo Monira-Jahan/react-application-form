bc0584df696248fad3a3d8a174dc6219c977d43f
Monira Jahan <monirajahan459@gmail.com>
2023-08-23 00:08:32 +0600

    Second Page-Component1 Added

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,19 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Post } from '../Model/models';
+import { fetchPosts } from '../Model/api';
+import { DataGrid } from '@mui/x-data-grid';
 
 const SecondPage = () => {
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'title', headerName: 'Title', width: 300 },
+        { field: 'body', headerName: 'Body', width: 600 },
+    ];
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const fetchedPosts = await fetchPosts();
+            setPosts(fetchedPosts);
+        })();
+    }, []);
     const userDetails = JSON.parse(localStorage.getItem('userDetails') || '{}');
     const navigate = useNavigate();
     useEffect(() => {
-       
         if (!userDetails) {
-          alert('Please provide your details first.');
-          navigate('/');
+            alert('Please provide your details first.');
+            navigate('/');
         }
-      }, []);
+    }, []);
+
     return (
         <div>
-           <h1>Welcome to the Second Page</h1> 
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid rows={posts} columns={columns} pageSizeOptions={[10]} />
+            </div>
         </div>
     );
 };
